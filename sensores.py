0194f56dfbebf91deb247ad50c00b60a102f6540
PolarisDB <99702849+PolarisDB@users.noreply.github.com>
2022-05-09 22:04:21 -0400

    Update and rename README.md to sensores.py

--- sensores.py
+++ sensores.py
@@ -1,11 +1,3 @@
-## Proyecto: Gestion de estacionamiento
-##Integrantes:
-##Lucio Aceval 3787999
-##Jun Galeano 4328172
-##Gabriel Mercado
-##Theo Clement Torres Ferraro 4572374
-##Francisco González 4304266
-
 import RPi.GPIO as GPIO
 import time
 
